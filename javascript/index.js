b782a4ee58e2a16616c9c79fc86f2c1fa793969f
nataliav30 <nataliav86@gmail.com>
2026-01-11 15:00:35 +0100

    Added functionality for the select JS

--- javascript/index.js
+++ javascript/index.js
@@ -19,5 +19,30 @@ frankfurtDateElement.innerHTML = frankfurtTime.format ("MMMM Do YYYY");
 frankfurtTimeElement.innerHTML = frankfurtTime.format( "HH:mm:ss");
 }
 
+function updateCity(event){
+let cityTimeZone=event.target.value;
+let cityName= cityTimeZone.split("/")[1];
+let cityTime= moment().tz(cityTimeZone);
+let citiesElement = document.querySelector("#cities");
+
+citiesElement.innerHTML=
+
+ `<div class ="city">
+<div>
+    <h2>${cityName}</h2>
+    
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+   
+    </div>
+   
+    <div class="time">${cityTime.format("HH:mm:ss")} </div>
+    </div>
+ </div>
+ `
+}
+
 updateTime();
 setInterval(updateTime,1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change",updateCity);
